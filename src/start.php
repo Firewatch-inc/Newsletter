db2c307b3816ed262212dbf539a582cb86bfa338
Sergey <pmswga@gmail.com>
2017-11-06 13:43:39 +0300

    Установка фреймворков и библиотек
    - Semantic
    - jQuery
    - Собственные классы (CForm, CTools, CTemplater)

---
+++ src/start.php
@@ -0,0 +1,8 @@
+<?php
+
+    require_once "engine/ctemplater.php";
+    require_once "engine/ctools.php";
+
+    $CT = new CTemplater("templates/tpl", "templates/tpl_c", "templates/configs", "templates/cache");
+
+?>
