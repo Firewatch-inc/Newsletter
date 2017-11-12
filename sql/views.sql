9838be9418b71e0ee477585d64b0fc76860046cb
Sergey <pmswga@gmail.com>
2017-11-13 00:53:40 +0400

    Небольшие исправления

--- sql/views.sql
+++ sql/views.sql
@@ -3,7 +3,7 @@ use newsletter;
 DROP VIEW IF EXISTS vNews;
 
 CREATE VIEW vNews(n_caption, n_content, n_author, n_date) 
-AS SELECT `caption`, `content`, `author`, `date` FROM `News`
+AS SELECT `caption`, `content`, `author`, `date` FROM `news`
 WHERE `date` >= NOW() OR `date` <= ADDDATE(NOW(), INTERVAL 7 DAY) 
 ORDER BY `date`;
 
