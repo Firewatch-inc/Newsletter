de05d340300ee125cf726b5e01a8546d5858923f
Sergey <pmswga@gmail.com>
2017-11-29 15:56:19 +0300

    Добавлены дополнительные данные

--- templates/tpl_c/3a8abe2d7919c9cac3f35a0cae6771eb988a358e_0.file.end.tpl.php
+++ templates/tpl_c/3a8abe2d7919c9cac3f35a0cae6771eb988a358e_0.file.end.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2017-11-21 12:26:19
+/* Smarty version 3.1.29, created on 2017-11-29 15:34:26
   from "C:\OpenServer\domains\Newsletter.mgutu\templates\tpl\html\end.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5a13f13b9c0914_32112730',
+  'unifunc' => 'content_5a1ea9522a3764_80958990',
   'file_dependency' => 
   array (
     '3a8abe2d7919c9cac3f35a0cae6771eb988a358e' => 
     array (
       0 => 'C:\\OpenServer\\domains\\Newsletter.mgutu\\templates\\tpl\\html\\end.tpl',
-      1 => 1511255434,
+      1 => 1511958864,
       2 => 'file',
     ),
   ),
@@ -19,8 +19,24 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5a13f13b9c0914_32112730 ($_smarty_tpl) {
+function content_5a1ea9522a3764_80958990 ($_smarty_tpl) {
 ?>
-    </body>
-</html><?php }
+    <?php echo '<script'; ?>
+ type="text/javascript">
+	
+		history.pushState(null, null, location.href);
+		window.onpopstate = function (event) {
+			history.go(1);	
+		};
+
+		window.oncontextmenu = function () {
+			return false;
+		};
+
+	<?php echo '</script'; ?>
+>
+
+	</body>
+</html>
+<?php }
 }
