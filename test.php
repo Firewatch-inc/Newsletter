34a7a76611dc18fe5ca0bf2c1f0b22dd5b10a8f2
Sergey <pmswga@gmail.com>
2017-12-21 08:11:42 +0300

    add all groups

--- test.php
+++ test.php
@@ -5,7 +5,7 @@
     $DB = new PDO("mysql:dbname=newsletter;host=localhost", "root", "57RbhCjy");
     $DB->exec("SET NAMES `utf-8`");
     
-    $xsl = PHPExcel_IOFactory::load("test.xlsx");
+    $xsl = PHPExcel_IOFactory::load("em.xlsx");
     $current_page = $xsl->getActiveSheet();
 
     $row = 12;
@@ -27,27 +27,25 @@
     echo "</thead>";
     echo "<tbody>";
     $count = 0;
-    for ($row = $row, $i = 1; ($current_page->getCell("B".$row) != ""); $row++) {
+    for ($row = $row, $i = 1; ($current_page->getCell("A".$row) != ""); $row++) {
         
-        $form_education_id = $DB->query("SELECT `id_education_form` FROM `ListOfEducationForm` WHERE `caption`='".(explode(" ", $current_page->getCell("F".$row))[0]."'"))->fetchAll()[0][0];
-        
-        $result *= $DB->exec("INSERT INTO `ListOfSpecialty` (`caption`, `code`) VALUES ('".trim($current_page->getCell("D".$row))."', '".$current_page->getCell("B".$row)."');");
+        $form_education_id = $DB->query("SELECT `id_education_form` FROM `ListOfEducationForm` WHERE `caption`='".(explode(" ", $current_page->getCell("E".$row))[0]."'"))->fetchAll()[0][0];
         
         for ($course = 1; $course < 7; $course++, $i++) {
-            if (preg_match("/^\d\d.\d\d.\d\d/", $current_page->getCell("B".$row))) {
-                                
+            if (preg_match("/^\d\d.\d\d.\d\d/", $current_page->getCell("A".$row))) {
+                
                 echo "<tr style='".$style."'>";
                 echo "<td>".$i."</td>";
+                echo "<td>".$current_page->getCell("A".$row)."</td>";
                 echo "<td>".$current_page->getCell("B".$row)."</td>";
                 echo "<td>".$current_page->getCell("C".$row)."</td>";
-                echo "<td>".$current_page->getCell("D".$row)."</td>";
                 echo "<td>".$form_education_id."</td>";
                 echo "<td>".$course."</td>";
-                echo "<td>1</td>";
+                echo "<td>5</td>";
                 echo "</tr>";
                 
                 $count += $DB->exec("
-                    INSERT INTO `Groups` (`caption`, `id_institute`, `education_direction`, `specialty`, `id_education_form`, `id_education_course`) VALUES ('".$current_page->getCell("B".$row)."', 1, '".$current_page->getCell("C".$row)."', '".$current_page->getCell("D".$row)."', '".$form_education_id."', ".$course.")
+                    INSERT INTO `Groups` (`caption`, `id_institute`, `education_direction`, `specialty`, `id_education_form`, `id_education_course`) VALUES ('".$current_page->getCell("A".$row)."', 5, '".$current_page->getCell("B".$row)."', '".$current_page->getCell("C".$row)."', '".$form_education_id."', ".$course.")
                 ");
                 
                 // print_r($DB->errorInfo());
@@ -59,6 +57,6 @@
     echo "</tbody>";
     echo "</table>";
     
-    echo "<h1>".$count."/252</h1>";
+    echo "<h1>".$count."/".$i."</h1>";
     
 ?>
